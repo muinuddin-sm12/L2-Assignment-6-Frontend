01412411b0896a477b397f3fc166d435b6e10b15
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-04-25 19:08:51 +0400

    add: Why Us section in home page

---
+++ src/app/(MainLayout)/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-[70vh] flex items-center justify-center'>
+        <h1 className='text-5xl text-center font-[600]'>No blog posted yet</h1>
+    </div>
+  )
+}
+
+export default page

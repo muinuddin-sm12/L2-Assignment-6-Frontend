65ada032253205c4f4040733a4b7704d6b8a0bd7
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-04-25 18:45:29 +0400

    add: banner section in home page

--- src/app/(MainLayout)/(home)/page.tsx
+++ src/app/(MainLayout)/(home)/page.tsx
@@ -1,9 +1,10 @@
+import BannerSection from "@/components/module/homepage/BannerSection";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div  className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "https://magazine.caser.es/wp-content/uploads/2020/06/Ni%C3%B1os-coman-verduras-825x483.jpg" }}>
-      Home Page
+      <BannerSection/>
     </div>
   );
 };
---
+++ src/components/module/homepage/BannerSection.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const BannerSection = () => {
+  return (
+    <div className="relative h-screen rounded-xl flex items-center justify-center overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-center blur-sm"
+        style={{ backgroundImage: "url('/bgImg.jpg')" }}
+      ></div>
+
+      {/* Content Layer */}
+      <div className="relative z-10 w-[500px] mx-auto text-center p-8 rounded-xl ">
+        <h1 className="text-6xl font-bold mb-4">
+          Healthy Eating <br /> for Busy People
+        </h1>
+        <p className="text-gray-700 mb-4">
+          Custom meal plans and personalized recipes — crafted to save your time
+          and help you eat better, every day.
+        </p>
+      <Link href={'/find-meals'}><Button className="bg-[#4CAF50] hover:bg-[#5ed662] px-6 py-6 text-base ">Explore Meals</Button></Link>
+      </div>
+
+    </div>
+  );
+};
+
+export default BannerSection;

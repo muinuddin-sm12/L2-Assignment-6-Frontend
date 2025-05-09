f6a74b9f90ffd73d043883b00784005bf4b9fa2b
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-05-09 20:32:04 +0400

    add: sticky navbar

--- src/app/(MainLayout)/layout.tsx
+++ src/app/(MainLayout)/layout.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const CommonLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-[1440px] mx-auto">
-      <div className="px-6 md:px-12 lg:px-20">
-      <Navbar/>
+    <div className="max-w-[1440px] mx-auto relative">
+      <div className="px-6 md:px-12 lg:px-20 sticky top-0 z-[999] bg-white">
+        <Navbar />
       </div>
       <main className="">{children}</main>
       <Footer />
